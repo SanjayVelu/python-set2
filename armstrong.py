99ea4bb818a91ebecc9b453ec44c87b045232b07
SanjayVelu <51081868+SanjayVelu@users.noreply.github.com>
2019-06-11 18:01:05 +0530

    Rename arm.py to armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,10 @@
+a,b=map(int(input().split())
+for num in range(a,b):
+  temp=num
+  sum=0
+  while temp>0:
+      digit=temp%10
+      sum=sum+digit**3
+      temp=temp//10
+      if sum==num:
+            print(num)
